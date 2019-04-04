6050ee4ede24f42ad4a3dbc59d667ffdcdd3539a
Максим Тимохин <maximtim@outlook.com>
2019-04-04 15:56:52 +0300

    registration

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    # ...
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'innprak-2019'
     USER = os.environ['POSTGRES_USER']
     PASSWORD = os.environ['POSTGRES_PASSWORD']
     HOST = os.environ['POSTGRES_HOST']
